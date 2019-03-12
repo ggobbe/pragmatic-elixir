d3b300c523919c365c6c165e93ed04c4b7c8dfa0
ggobbe <guillaume.gobbe@gmail.com>
2019-03-12 16:28:12 +0100

    Update solution according to book's solution

--- 03-modules/06-chop-guess.exs
+++ 03-modules/06-chop-guess.exs
@@ -1,19 +1,12 @@
-defmodule Chop do
-    def guess(x, actual, _low..high) when x < actual do
-        guess(actual, x..high)
-    end
-    def guess(x, actual, low.._high) when x > actual do
-        guess(actual, low..x)
-    end
-    def guess(x, actual, _range) when x === actual do 
-        IO.puts x
-    end
-    def guess(actual, range) do
-        low..high = range
-        target = low + div((high - low), 2)
-        IO.puts "Is it #{target}"
-        guess(target, actual, low..high)
-    end
+defmodule Chop 
+	do def guess(actual, range = low..high) do
+		guess = div(low + high, 2)
+		IO.puts "Is it #{guess}"
+		guess(actual, guess,range)
+	end
+	def guess(actual, actual, _), do: actual
+	def guess(actual, guess, _low..high) when guess < actual, do: guess(actual, guess+1..high)
+	def guess(actual, guess, low.._high) when guess > actual, do: guess(actual, low..guess-1)
 end
 
 Chop.guess(273, 1..1000)
